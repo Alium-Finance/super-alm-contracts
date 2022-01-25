875f7aaa845d99168dfb9704cc403c34ec3477d0
gfijrb <gfijrb@yandex.ua>
2022-01-25 18:31:24 +0200

    add IERC721Receiver support for safe storage

--- contracts/dao/storage/SafeStorage.sol
+++ contracts/dao/storage/SafeStorage.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.8.0;
 
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/introspection/ERC165Storage.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -60,6 +61,7 @@ contract SafeStorage is ERC165Storage, Ownable, ERC721Holder, ERC1155Holder {
         returns (bool)
     {
         return
+            interfaceId == type(IERC721Receiver).interfaceId ||
             ERC1155Receiver.supportsInterface(interfaceId) ||
             ERC165Storage.supportsInterface(interfaceId);
     }
